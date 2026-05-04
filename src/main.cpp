3f477cdd5a47f38844cb6ce8f97cbff91a34e264
JohnDRubio <rubiojohn717@gmail.com>
2026-05-03 23:40:41 -0700

    Blank canvas for rasterizer

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,23 @@
 #define CANVAS_WIDTH 1600
 #define CANVAS_HEIGHT 900
 
-#ifdef _cplusplus
-extern "C" {
-#endif
+int boundX(int x) {
+	if (x < 0) return 0;
+	if (x >= CANVAS_WIDTH) return CANVAS_WIDTH - 1;
+	return x;
+}
+int boundY(int y) {
+	if (y < 0) return 0;
+	if (y >= CANVAS_HEIGHT) return CANVAS_HEIGHT - 1;
+	return y;
+}
+void PutPixel(int x, int y, const Color color) {
+	// Translate from Cartesian coordinates to screen coordinates
+	int Cx = boundX(x + CANVAS_WIDTH / 2);
+	int Cy = boundY((-y) + CANVAS_HEIGHT / 2);
+	DrawPixel(Cx, Cy, color);
+}
+
 int main(void) {
 	// init app
 	InitWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "First Raylib App");
@@ -15,13 +29,10 @@ int main(void) {
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BACKGROUND_COLOR);
-		DrawPixel(400, 300, RED);
+		PutPixel(0, 0, RED);
 		EndDrawing();
 	}
 	// close app
 	CloseWindow();
 	return 0;
 }
-#ifdef _cplusplus
-}
-#endif
