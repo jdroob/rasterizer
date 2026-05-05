15961e5f1bc6f9eec4d1325a2ae7af00e280bf4c
JohnDRubio <rubiojohn717@gmail.com>
2026-05-05 00:34:21 -0700

    Add DrawLine()

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,58 @@
 #include <cstdio>
+#include <cmath>
+#include <functional>
 #include "raylib.h"
 
 #define BACKGROUND_COLOR SKYBLUE
 #define CANVAS_WIDTH 1600
 #define CANVAS_HEIGHT 900
+
+/**
+ * Rasterization Notes:
+ *  Let's get the line math out of the way:
+ * 		- Given 2 points: P0 and P1, represent the line: P0->P1 as 
+ *      P = P0 + t(P1 - P0)
+ *      
+ * 			where P is any point on the line P0->P1.
+ * 
+ *    - Decompose the above into
+ * 		 x = x0 + t(x1 - x0)
+ *     y = y0 + t(y1 - y0)
+ * 
+ *     then
+ *     t = (x - x0) / (x1 - x0)
+ *     => y = y0 + [(x-x0)/(x1-x0)] * (y1-y0)
+ *     => y = y0 + (x-x0)[(y1-y0) / (x1-x0)]
+ * 
+ *    Notice 
+ *     [(y1-y0) / (x1-x0)] is a constant that simply depends on the endpoints
+ *     so let's call this conastant `a`.
+ * 
+ * 	   => y = y0 + a(x-x0)   // Note: a is really the slope of the line
+ *     => y = y0 + ax - ax0
+ *     => y = ax + y0-ax0
+ * 
+ *    Let b := y0-ax0 (since y0-ax0 is simply a constant defined by the line's endpoints)
+ *    => y = ax + b.
+ * 
+ *   In summary, we now have our handy dandy linear formula: y = ax + b.
+ *   Note that this can define any line EXCEPT vertical lines b/c that'd imply x0 == x1
+ *   and therefore we'd have a div by zero. So we'll ignore those for now.
+ * 
+ *  So our pseudo-code for DrawLine is:
+ *  
+ *  DrawLine(P0, P1, Color) {
+ * 	a = (y1-y0) / (x1-x0);
+ *    b = (y0 - a * x0);
+ *    for x in x0 to x1:
+ * 			y = a * x + b;
+ *      DrawLine(x, y, Color);
+ *  }
+ */
+
+ struct Point_t {
+	float x, y;
+ };
 
 int boundX(int x) {
 	if (x < 0) return 0;
@@ -22,6 +71,51 @@ void PutPixel(int x, int y, const Color color) {
 	DrawPixel(Cx, Cy, color);
 }
 
+//  void ras_DrawLine(Point_t P0, Point_t P1, Color color) {
+// 	float a = (P1.y - P0.y) / (P1.x - P0.x);
+// 	float b = P0.y - a * P0.x;
+// 	float x = P0.x;
+// 	float dx = P1.x - P0.x > 0 ? 0.1 : -0.1;
+// 	std::function<bool(float)> isDone = [&P1](float x) { return x >= P1.x; };
+// 	if (dx < 0) {
+// 		isDone = [&P1](float x) { return x <= P1.x; };
+// 	}
+// 	std::function<void(float&)> iterate = [](float& x) { ++x; };
+// 	if (dx < 0) {
+// 		iterate = [](float& x) { --x; };
+// 	}
+// 	while (!isDone(x)) {
+// 		float y = a * x + b;
+// 		PutPixel(x, y, color);
+// 		iterate(x);
+// 	}
+//  }
+
+ void ras_DrawLine(Point_t P0, Point_t P1, Color color) {
+    if (P0.x > P1.x) {
+        std::swap(P0, P1);
+    }
+
+	// vertical line test
+    if (P0.x == P1.x) {
+        float y0 = std::min(P0.y, P1.y);
+        float y1 = std::max(P0.y, P1.y);
+        for (float y = y0; y <= y1; y += 0.1f) {
+            PutPixel((int)P0.x, (int)y, color);
+        }
+        return;
+    }
+
+    float a = (P1.y - P0.y) / (P1.x - P0.x);
+    float y = P0.y;
+    float step = 1.f;
+
+    for (float x=P0.x; x<=P1.x; x+=step) {
+        PutPixel((int)std::round(x), (int)std::round(y), color);
+        y += a;
+    }
+}
+
 int main(void) {
 	// init app
 	InitWindow(CANVAS_WIDTH, CANVAS_HEIGHT, "First Raylib App");
@@ -29,7 +123,7 @@ int main(void) {
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BACKGROUND_COLOR);
-		PutPixel(0, 0, RED);
+		ras_DrawLine({400,-200}, {-400,200}, RED);
 		EndDrawing();
 	}
 	// close app
